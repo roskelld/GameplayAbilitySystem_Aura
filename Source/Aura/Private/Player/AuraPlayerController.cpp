e6ddedf3134e7c3eaa2b44e06ff91b4ee2294299
Dean Roskell <roskelld@gmail.com>
2023-10-16 13:19:53 -0600

    EnhancedInput Controller Plugin
    Set up the EnhancedInput Controller.

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,5 +1,31 @@
 // Copyright Iron Goblin Corp. 2023
 
-
+#include "EnhancedInputSubsystems.h"
 #include "Player/AuraPlayerController.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Assert if not setup
+	check(AuraContext);
+
+	// Enhanced Input Mapping Context
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	// Mouse Cursor Setup
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
