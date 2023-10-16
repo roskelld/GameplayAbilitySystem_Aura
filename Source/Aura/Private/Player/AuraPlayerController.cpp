c9a46339c7a9e9ff8129860eba786ee729b3b0fa
Dean Roskell <roskelld@gmail.com>
2023-10-16 12:56:43 -0600

    Set up basic move set and player controller class

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright Iron Goblin Corp. 2023
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Iron Goblin Corp. 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
