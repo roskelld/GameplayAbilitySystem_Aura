5ca76d94d66c128c164c60005ad52aa0245b5b49
Dean Roskell <roskelld@gmail.com>
2023-10-18 10:54:24 -0600

    Set attribute defaults
    Used the provided macros to setup attribute functions.

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -1,12 +1,14 @@
 // Copyright Iron Goblin Corp. 2023
 
-#include "AbilitySystemComponent.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "Net/UnrealNetwork.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(50.f);
+	InitMaxMana(50.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
