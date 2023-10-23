496e76e2e9d8412f83e565a6e8d71908728cbfe7
Dean Roskell <roskelld@gmail.com>
2023-10-23 12:16:15 -0600

    AssetTag event broadcast base code
    Delegate function that will broadcast GE asset tags to associated objects.
    
    This version only prints tag to screen.

--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -18,5 +18,12 @@ public:
 
 protected:
 
+	/* 
+	 Broadcast event messages via Tags
+	 Using the GameplayEffectAssetTags (Added in the GE) we can broadcast them to any associated deligate
+	 such as UI widgets, so they can display appropreate user updates.
+	 The useful concept here is that these tags are not applied to the character, but stay on the GE, *and*
+	 they work on instant effects too, sending the tag out before removing it. So great for flash events. 
+	*/
 	void EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle);
 };
