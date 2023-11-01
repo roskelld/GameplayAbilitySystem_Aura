adc747e8083aa1d61256f992e7c7794cfa01ba86
Dean Roskell <roskelld@gmail.com>
2023-11-01 16:27:26 -0600

    Send task data to server with prediction (lesson 117)

--- Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
@@ -2,6 +2,7 @@
 
 
 #include "AbilitySystem/AbilityTask/TargetDataUnderMouse.h"
+#include "AbilitySystemComponent.h"
 
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
@@ -12,8 +13,42 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+
+	if (bIsLocallyControlled)
+	{
+		// local client will be able to get data from their controller
+		SendMouseCursorData();
+	}
+	else 
+	{
+		//TODO: We are the server and need to listen for target data to arrive
+	}
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	// Tell server that this logic will be predicted on the client and can run
+	// but will be subject to server authoritive changes should things not align
+	FScopedPredictionWindow ScopedPrediction(AbilitySystemComponent.Get());
+
+	// Get Cursor hit info from player controller
 	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
 	FHitResult CursorHit;
 	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
-	ValidData.Broadcast(CursorHit.Location);
+	
+	// Generate datahanle type and populate with vector data for location
+	FGameplayAbilityTargetDataHandle DataHandle;
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult = CursorHit;
+	DataHandle.Add(Data);
+
+	// Replicate data to server
+	AbilitySystemComponent->ServerSetReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey(), DataHandle, FGameplayTag(), AbilitySystemComponent->ScopedPredictionKey);
+
+	// Broadcast the data
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
 }
