8653db3d11e4626619f6a21cd91c4f8789193657
Dean Roskell <roskelld@gmail.com>
2023-11-14 18:08:35 -0700

    Custom Behavior tree service set up (lesson 165)
    No functionality as of yet.

---
+++ Source/Aura/Public/AI/BTService_FindNearestPlayer.h
@@ -0,0 +1,22 @@
+// Copyright Iron Goblin Corp. 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlueprintBase.h"
+#include "BTService_FindNearestPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UBTService_FindNearestPlayer : public UBTService_BlueprintBase
+{
+	GENERATED_BODY()
+	
+public:
+
+protected:
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
