46e504c7a5d0ff3905505e53fb63d507e4840710
Dean Roskell <roskelld@gmail.com>
2023-11-16 11:42:01 -0700

    Organize GAS folders and set up projectile attack (lesson 190)

---
+++ enc_temp_folder/6a9d1eb742e04b43c1321e33c5f2ca/AuraProjectileSpell.cpp
@@ -0,0 +1,68 @@
+// Copyright Iron Goblin Corp. 2023
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+#include "Actor/AuraProjectile.h"
+#include "AbilitySystemComponent.h"
+#include "Interaction/CombatInterface.h"
+#include <AbilitySystemBlueprintLibrary.h>
+#include "AuraGameplayTags.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+
+void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation)
+{
+	// Check that we have authority over this ability 
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
+
+	// Exit if we are not the server
+	if (!bIsServer) return;
+
+	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), FAuraGameplayTags::Get().Montage_Attack_Weapon);
+	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+
+	FTransform SpawnTransform;
+	SpawnTransform.SetLocation(SocketLocation);
+	SpawnTransform.SetRotation(Rotation.Quaternion());
+
+	// Prepare projectile actor ready for spawn
+	AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(ProjectileClass, SpawnTransform, GetOwningActorFromActorInfo(), Cast<APawn>(GetOwningActorFromActorInfo()), ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+	// Get an ASC to generate data
+	const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
+
+	FGameplayEffectContextHandle EffectContextHandle = SourceASC->MakeEffectContext();
+
+	/* Here we create an EffectContextHandle and fill out some of the optional data that it can carry to help show off what is built into the system already 
+	This information isn't needed for the spell, but is good knowledge all the same */
+
+	EffectContextHandle.SetAbility(this);
+	EffectContextHandle.AddSourceObject(Projectile);
+	TArray<TWeakObjectPtr<AActor>> Actors;
+	Actors.Add(Projectile);
+	EffectContextHandle.AddActors(Actors);
+	FHitResult HitResult;
+	HitResult.Location = ProjectileTargetLocation;
+	EffectContextHandle.AddHitResult(HitResult);
+
+	// Use ASC to create a spec handle that will be carried by the projectile
+	const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), EffectContextHandle);
+
+	// Set by Caller Damage Amount
+	const FAuraGameplayTags GameplayTags = FAuraGameplayTags::Get();
+
+	for (auto& Pair : DamageTypes)
+	{
+		const float ScaledDamage = Pair.Value.GetValueAtLevel(GetAbilityLevel());
+		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, Pair.Key, ScaledDamage);
+	}
+
+	// Apply the spec handle to the damage effect on AuraProjectile
+	Projectile->DamageEffectSpecHandle = SpecHandle;
+
+	Projectile->FinishSpawning(SpawnTransform);
+}
