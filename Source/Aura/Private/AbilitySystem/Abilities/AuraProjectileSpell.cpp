47323fcb00677dc348e2a181276a83bff5c1121e
Dean Roskell <roskelld@gmail.com>
2023-11-19 22:24:36 -0700

    Decrementing Minion Count (lesson 227)

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -14,7 +14,7 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 }
 
 
-void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag)
+void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride)
 {
 	// Check that we have authority over this ability 
 	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
@@ -24,9 +24,11 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 
 	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), SocketTag);
 	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+	if (bOverridePitch) Rotation.Pitch = PitchOverride;
 
 	FTransform SpawnTransform;
 	SpawnTransform.SetLocation(SocketLocation);
+	
 	SpawnTransform.SetRotation(Rotation.Quaternion());
 
 	// Prepare projectile actor ready for spawn
