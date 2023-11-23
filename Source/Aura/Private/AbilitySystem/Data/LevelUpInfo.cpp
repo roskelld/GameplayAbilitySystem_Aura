25cab64ef88c1fc21b63ac72adc8457a9873ccb0
Dean Roskell <roskelld@gmail.com>
2023-11-23 14:08:04 -0700

    Level Up Info Data Asset (lesson 250)

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,26 @@
+// Copyright Iron Goblin Corp. 2023
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		// We define level to match the array index, meaning 0 index is 
+		// skippped and minimum is 1
+		if (LevelUpInformation.Num() - 1 <= Level) return Level;
+
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
