988eb2bb1f8ebaa679a8aa711a4eb72b69545f24
Dean Roskell <roskelld@gmail.com>
2023-12-06 15:31:32 -0700

    Aura Beam Spell (lesson 321)

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -0,0 +1,26 @@
+// Copyright Iron Goblin Corp. 2023
+
+
+#include "AbilitySystem/Abilities/AuraBeamSpell.h"
+
+void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
+{
+	if (HitResult.bBlockingHit)
+	{
+		MouseLocation = HitResult.ImpactPoint;
+		MouseHitActor= HitResult.GetActor();
+	} 
+	else
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true);
+	}
+}
+
+void UAuraBeamSpell::StoreOwnerPlayerController()
+{
+	if (CurrentActorInfo)
+	{
+		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
+	}
+	
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -0,0 +1,33 @@
+// Copyright Iron Goblin Corp. 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AuraBeamSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraBeamSpell : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void StoreMouseDataInfo(const FHitResult& HitResult);
+
+	UFUNCTION(BlueprintCallable)
+	void StoreOwnerPlayerController();
+
+protected:
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	FVector MouseLocation;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<AActor> MouseHitActor;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<APlayerController> OwnerPlayerController;
+};
